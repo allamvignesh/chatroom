fef69db92036699fa91cef3e84fc39bba63e6cc0
Vignesh Allam <allam.vignesh@gmail.com>
2023-01-14 17:56:54 +0530

    initial commit

---
+++ static/index.js
@@ -0,0 +1,53 @@
+let socket = io()
+let login = document.getElementById('login');
+let messages = document.getElementById('chat');
+
+
+if (login != null){
+    login.onclick = function(){
+        console.log("logging in")
+        socket.connect();
+    }
+};
+
+socket.on('connect', function(){
+    let name = document.getElementById("name");
+    console.log("connecting")
+    socket.emit('notify', name.value + " joined!");
+});
+
+socket.on('notify', function(status){
+    console.log(status, "notified");
+});
+
+socket.on('returndata', function(data){
+
+    if (data['name'] != document.title.split(" ")[1]){
+        var content = 
+            '<div class=""><strong>' + data['name'] + '</strong></div>' + 
+            '<div class="mx-3 px-3 word-wrap rounded-lg bg-blue-200 w-fit">' + data['message'] + '</div>'
+    } else {
+        var content = 
+            '<div class="text-right"><strong>You</strong></div>' + 
+            '<div class="ml-auto text-right mx-3 px-3 word-wrap rounded-lg bg-blue-200 w-fit">' + data['message'] + '</div>'
+    }
+
+    divtag = document.createElement('div');
+    divtag.innerHTML = content;
+    messages.appendChild(divtag)
+});
+
+let send = document.getElementById("sendmsg");
+
+if (send != null){
+    send.onclick = function(){
+        msg = document.getElementById("message")
+        if (msg.value != ""){
+            socket.emit('data', {
+                message: msg.value,
+                name: document.title.split(" ")[1]
+            })
+        }
+        msg.value = ""
+    }
+};
